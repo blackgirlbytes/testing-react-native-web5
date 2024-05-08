94423da5269fa3ad5b9e1259814ede70bd316bf9
Rizel <rizel@tbd.email>
2024-05-08 12:30:56 -0400

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-private-methods', {loose: true}],
+    [
+      'module-resolver',
+      {
+        alias: {
+          crypto: 'react-native-quick-crypto',
+          stream: 'readable-stream',
+          buffer: '@craftzdog/react-native-buffer',
+          'bn.js': 'react-native-bignumber',
+        },
+      },
+    ],
+  ],
 };
